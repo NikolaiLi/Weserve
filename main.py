13d3071b8c01a803e9b78a015b90683eeac47b67
Ata12333 <118903521+Ata12333@users.noreply.github.com>
2024-05-13 08:36:57 +0200

    login system

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from sanic import Sanic
 from sanic_jinja2 import SanicJinja2
+from sanic.response import redirect
+import uuid
 
 app = Sanic("Try")
 app.static("/static", "./static")
 jinja = SanicJinja2(app)
 
+<<<<<<< Updated upstream
 baner = ["Tennis","Badminton","Volleyball"]
 
 globals ={ 
@@ -12,6 +15,12 @@ globals ={
            "menu": {"Book":"/book","Bliv Medlem":"/logind",}
 =======
            "menu": {"Book":"/book", "Bliv Medlem":"/logind"},
+>>>>>>> Stashed changes
+=======
+Bliv_Medlem = []
+
+globals ={ 
+           "menu": {"Book":"/book","Bliv Medlem":"/logind"}
 >>>>>>> Stashed changes
          }
 
@@ -30,10 +39,22 @@ async def book(request):
 async def logind(request):
     return globals
 
-@app.get("/Bliv_medlem", name = "Bliv_medlem-page")
-@jinja.template("Bliv_medlem.html")
-async def Bliv_medlem(request):
+@app.get("/logpå")
+@jinja.template("logpå.html")
+async def logpå(request):
     return globals
+
+@app.post("/logind")
+async def logind(request):
+    Bliv_Medlem_brugernavn = request.form.get("Brugernavn")
+    Bliv_Medlem_gmail = request.form.get("Gmail")
+    Bliv_Medlem_adgangskode = request.form.get("Adgangskode")
+    id = str(uuid.uuid4())
+
+    Bliv_Medlem_entry = {"id": id, "Bliv_Medlem_brugernavn": Bliv_Medlem_brugernavn, "Bliv_Medlem_gmail": Bliv_Medlem_gmail, "Bliv_Medlem_adgangskode": Bliv_Medlem_adgangskode}
+    Bliv_Medlem.append(Bliv_Medlem_entry)
+    return redirect("/login")
+
 
 if __name__ == "__main__":
     app.run(host="localhost", port=8080)
