edf10b874a1272a3cbe7b0e1a20488c1f0f9daa2
Ata12333 <118903521+Ata12333@users.noreply.github.com>
2024-05-13 11:13:03 +0200

    login

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from sanic import Sanic
 from sanic_jinja2 import SanicJinja2
+import uuid
+from sanic.response import redirect 
 
 app = Sanic("Try")
 app.static("/static", "./static")
@@ -7,8 +9,11 @@ jinja = SanicJinja2(app)
 
 baner = ["Tennis","Badminton","Volleyball"]
 
+Bliv_medlem = []
+
 globals ={ 
-           "menu": {"Book":"/book", "Bliv Medlem":"/logind"},
+           "menu": {"Book":"/book", "login":"/logind"},
+           "Bliv_medlem" : {},
          }
 
 @app.get("/")
@@ -26,10 +31,23 @@ async def book(request):
 async def logind(request):
     return globals
 
-@app.get("/Bliv_medlem", name = "Bliv_medlem-page")
-@jinja.template("Bliv_medlem.html")
+@app.get("/Bliv_medlem", name = "medlem-page")
+@jinja.template("signup.html")
 async def Bliv_medlem(request):
     return globals
+
+@app.post("/Bliv_medlem")
+async def Bliv_medlem(request):
+    Bliv_medlem_brugernavn = request.form.get("Brugernavn")
+    Bliv_medlem_gmail = request.form.get("Gmail")
+    Bliv_medlem_adgangskode = request.form.get("Adgangskode")
+    id = str(uuid.uuid4())
+
+    Bliv_medlem_entry = {"id": id, "Bliv_medlem_brugernavn": Bliv_medlem_brugernavn, "Bliv_medlem_gmail": Bliv_medlem_gmail, "Bliv_medlem_adgangskode": Bliv_medlem_adgangskode}
+    Bliv_medlem.append(Bliv_medlem_entry)
+    return redirect("/logind")
+
+
 
 if __name__ == "__main__":
     app.run(host="localhost", port=8080)
