b17bb63eb42b4dc1ee2a49ea9a1754c5bd9afb81
Ata12333 <118903521+Ata12333@users.noreply.github.com>
2024-05-14 20:37:54 +0200

    login

--- main.py
+++ main.py
@@ -41,20 +41,35 @@ async def sport(request, sport_valg: str):
             print("ikke en rigtig sport")
     return globals
 
-@app.get("/book")
-@jinja.template("book.html")
-async def book(request):
-    return globals
-
 @app.get("/logind")
 @jinja.template("logind.html")
 async def logind(request):
+    if globals["current_user"]:
+        return globals
+    else:
+        globals_copy = globals.copy()
+        if "Bliv Medlem" in globals_copy["menu"]:
+            globals_copy["menu"].pop("Bliv Medlem")
+        globals_copy["menu"]["Bliv Medlem"] = "/Bliv_medlem"
+        return globals_copy
+
+@app.get("/book")
+@jinja.template("book.html")
+async def book(request):
     return globals
 
 @app.get("/Bliv_medlem", name = "medlem-page")
 @jinja.template("Bliv_medlem.html")
 async def bliv_medlem(request):
     return globals
+
+@app.get("/logaf/<brugernavn>")
+async def log_af(request, brugernavn: str):
+    if globals["current_user"] == brugernavn:
+        globals["current_user"] = None
+        if f"log af: {brugernavn}" in globals["menu"]:
+            globals["menu"].pop(f"log af: {brugernavn}")
+        return redirect("/logind")
 
 @app.post("/valgt")
 async def valgt_dato(request):
@@ -73,8 +88,8 @@ async def login(request):
     
     if match and match["Bliv_medlem_adgangskode"] == adgangskode:
         globals["current_user"] = brugernavn
-        globals["menu"].pop("Bliv Medlem")
-        globals["menu"]["log af: " + brugernavn] = "/logind"
+        if "Bliv Medlem" in globals["menu"]:
+            globals["menu"].pop("Bliv Medlem")
         redirect_obj = redirect("/")
         return redirect_obj
     else:
